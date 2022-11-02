55facb098c76b850dc8f66c68aea95215fb85296
ashish <ashishvj123@gmail.com>
2022-11-02 23:32:11 +0530

    a

---
+++ singleprint.java
@@ -0,0 +1,11 @@
+class singleprint
+{	
+	public static void main(String[] args) 
+	{
+		String a = ("hi 'Ratan' sir great /mango/ is good \nOrange is fruit 'Apple' is great\ntoday\tclass\tcompleted");
+		System.out.println(a); 		
+	}
+}
+
+
+
